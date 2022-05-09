16b591b0d6b030f544ac3173c86981b97dd702c1
Ralph Cajipe <ralphcajipe@gmail.com>
2022-05-09 18:24:12 +0800

    Creating Column Vectors

---
+++ 2/column1.m
@@ -0,0 +1,6 @@
+% Creating Column Vectors
+
+% Explicitly put the values in square brackets, separated by 
+% semicolons
+c = [1; 2; 3; 4];
+disp(c);
---
+++ 2/column2.m
@@ -0,0 +1,9 @@
+% Creating Column Vectors
+
+row = 1:3;
+disp(row);
+
+% Transpose row into column by apostrophe '
+column = row';
+disp(column);
+

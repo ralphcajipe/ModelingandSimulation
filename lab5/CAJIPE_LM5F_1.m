84b32251289f78edf33a30826a493e1ed44354be
Ralph Cajipe <ralphcajipe@gmail.com>
2022-06-18 10:17:21 +0800

    add lab5

---
+++ lab5/CAJIPE_LM5F_1.m
@@ -0,0 +1,16 @@
+%{
+    Create a MATLAB script for the following problem:
+
+    Write a MATLAB function that will plot cos(x) for x values ranging from –pi to pi 
+    in steps of 0.1, using black stars (*). 
+    It will do this three times across in one Figure Window, with varying line widths. 
+
+    (Note: Even if individual points are plotted rather than a solid line, 
+    the line width property will change the size of these points.) 
+    If no arguments are passed to the function, the line widths will be 1, 2, and 3. 
+
+    elseif, an argument is passed to the function, 
+    it is a multiplier for these values 
+    (e.g., if 3 is passed, the line widths will be 3, 6, and 9). 
+    The line widths will be printed in the titles on the plots.
+%}
